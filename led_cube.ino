ec58baa06cb23e6a084ccf4aace59d62090498e8
Joshua Leger <legermjoshua@gmail.com>
2019-10-09 18:00:35 -0700

    Set up basic loop for showing animations

--- led_cube.ino
+++ led_cube.ino
@@ -1,42 +1,89 @@
-int levels[4] = {46,47,48,49};
-int locs[16] = {30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45};
+// Cube size
+int xSize = 4;
+int ySize = 4;
+int zSize = 4;
+// Layers from top to bottom
+int layers[4] = {46,47,48,49};
+// xy plane, with x first
+int xy[4][4] = {
+  {30,31,32,33},
+  {34,35,36,37},
+  {38,39,40,41},
+  {42,43,44,45}
+};
+// The next frame, to be filled up
+int next[4][4][4] = {
+  {
+    {HIGH,HIGH,HIGH,HIGH},
+    {HIGH,LOW,LOW,HIGH},
+    {HIGH,LOW,LOW,HIGH},
+    {HIGH,HIGH,HIGH,HIGH}
+  },
+  {
+    {HIGH,LOW,LOW,HIGH},
+    {LOW,LOW,LOW,LOW},
+    {LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,HIGH}
+  },
+  {
+    {HIGH,LOW,LOW,HIGH},
+    {LOW,LOW,LOW,LOW},
+    {LOW,LOW,LOW,LOW},
+    {HIGH,LOW,LOW,HIGH}
+  },
+  {
+    {HIGH,HIGH,HIGH,HIGH},
+    {HIGH,LOW,LOW,HIGH},
+    {HIGH,LOW,LOW,HIGH},
+    {HIGH,HIGH,HIGH,HIGH}
+  }
+};
+// Time when frame started
+int frameTime = 0;
+// To keep track of current layer lit up
+int on = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  for(int i = 30; i < 50; i++) {
-    pinMode(i, OUTPUT);
+  // Set up layers
+  for(int z = 0; z < zSize; z++) {
+    pinMode(layers[z], OUTPUT);
+    // Set as high to have all the lights off
+    digitalWrite(layers[z], HIGH);
   }
-  digitalWrite(40,HIGH);
-  digitalWrite(46,HIGH);
-  digitalWrite(48,HIGH);
-  digitalWrite(47,HIGH);
-}
 
-void turnOnLevel(int level) {
-  for(int i = 0; i < 4; i++) {
-    if(i == level) {
-      digitalWrite(levels[i],LOW);
-    }
-    else {
-      digitalWrite(levels[i],HIGH);
+  // Set up xy plane
+  for(int x = 0; x < xSize; x++) {
+    for(int y = 0; y < ySize; y++) {
+      pinMode(xy[x][y], OUTPUT);
     }
   }
 }
 
-void resetLocs() {
-  for(int i = 0; i < 16; i++) {
-    digitalWrite(locs[i],LOW);
-  }
+// Turns off current layer and sets up next one
+void cycleDownLayer() {
+  digitalWrite(layers[on], HIGH);
+  on = (on + 1) % zSize;
+}
+
+// Turns the next layer on, once xy is ready
+void cycleUpLayer() {
+  digitalWrite(layers[on], LOW);
+}
+
+// Refills next frame;
+void fillOutNextFrame() {
+  frameTime = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for(int i = 0; i < 4; i++) {
-    for(int j = 0; j < 16; j++) {
-      turnOnLevel(i);
-      resetLocs();
-      digitalWrite(locs[j],HIGH);
-      delay(1000);
+  on = zSize - 1;
+  for(int z = 0; z < zSize; z++) {
+    cycleDownLayer();
+    for(int x = 0; x < xSize; x++) {
+      for(int y = 0; y < ySize; y++) {
+        digitalWrite(xy[x][y],next[z][x][y]);
+      }
     }
+    cycleUpLayer();
   }
 }
